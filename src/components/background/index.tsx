78fcf7de63b9bc4feff1b5c39f8f4189b4ed7bde
justinlee05 <justinlee05@naver.com>
2023-01-15 07:16:51 +0900

    34

--- src/components/background/index.tsx
+++ src/components/background/index.tsx
@@ -12,8 +12,7 @@ function Background({ children }: Props) {
 export default Background;
 
 const TotalWrapper = styled.div`
-  border: 1px solid black;
-  width: 100%;
-  height: 100vh;
+  width: 390px;
+  height: 844px;
   position: relative;
 `;
